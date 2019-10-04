0bf73775c6138e673c38679a30314682ae14d6a3
Juan Carlo A. Banayo <johncban@gmail.com>
2019-10-04 13:04:56 -0400

    Update scraper with date_stamp and comparison
    Add date_stamp for the article post with a comparison operator to list the newest article on the top.

--- lib/newsjournal/newsarticle.rb
+++ lib/newsjournal/newsarticle.rb
@@ -1,25 +1,23 @@
 class Newsjournal::NewsArticle
 
-    attr_reader :article, :url, :sum, :date_auth
+    attr_reader :article, :url, :sum, :date_auth, :date_stamp
     attr_accessor :full
 
     @@articles = []
     
-    def self.start_scrape
-        Newsjournal::NewsScraper.get_articles
-    end
-    
-    def self.articles
-        @@articles
-    end
-
-    def initialize(article, url, full, sum, date_auth)
+    def initialize(article, url, full, sum, date_auth, date_stamp)
         @article = article
         @url = url
         @full = full
         @sum = sum 
         @date_auth = date_auth
+        @date_stamp = date_stamp
         @@articles << self
+    end
+    
+
+    def self.articles
+        @@articles.sort! { |ar1, ar2| ar2.date_stamp <=> ar1.date_stamp }
     end
 
 end
